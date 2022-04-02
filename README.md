43bfd94b8ac37b9df3661a5693cb01312a0555fc
robin@mordasiewicz.com <robin@mordasiewicz.com>
2022-04-02 10:55:07 -0400

    creating skel

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# jenkins-container
+# jenkins
---
+++ increment-version.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+
+curl -s -L https://updates.jenkins.io/stable/latestCore.txt --output JENKINS_VERSION
+
+JENKINS_VERSION=`cat JENKINS_VERSION`
+LOCALREVISION=`cat VERSION | sed -re "s/^[0-9]+\.[0-9]+\.[0-9]+-*([0-9]*)/\1/"`
+
+LOCALREVISION=`echo $LOCALREVISION | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}'`
+echo "${JENKINS_VERSION}-${LOCALREVISION}" > VERSION
+
+cat Dockerfile | sed -re "s/ENV JENKINS_VERSION.*/ENV JENKINS_VERSION ${JENKINS_VERSION}-${LOCALREVISION}/" > Dockerfile.tmp && mv Dockerfile.tmp Dockerfile
