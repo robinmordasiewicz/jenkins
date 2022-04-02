a9038e80ba8cc5e08748dbcbc3ca283e7b4be449
robin@mordasiewicz.com <robin@mordasiewicz.com>
2022-04-02 15:11:54 -0400

    creating skel

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #
 
-docker build -t robinhoodis/jenkins:2.332.1-12 .
-docker push robinhoodis/jenkins:2.332.1-12
+docker build -t robinhoodis/jenkins:2.332.1-13 .
+docker push robinhoodis/jenkins:2.332.1-13
