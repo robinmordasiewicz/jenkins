08ee09f04fb1c42f20d1c74fac3a09d51abbb9fa
Robin Mordasiewicz <robin@mordasiewicz.com>
2022-04-03 05:04:34 +0000

    2.332.1-39

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #
 
-docker build -t robinhoodis/jenkins:2.332.1-38 .
-docker push robinhoodis/jenkins:2.332.1-38
+docker build -t robinhoodis/jenkins:2.332.1-39 .
+docker push robinhoodis/jenkins:2.332.1-39
