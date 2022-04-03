f9503a51ccdd7a8e8bef9b01556f56b5d442db98
robin@mordasiewicz.com <robin@mordasiewicz.com>
2022-04-03 12:42:06 -0400

    creating skel

--- increment-version.sh
+++ increment-version.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #
 
-curl -s -L https://updates.jenkins.io/stable/latestCore.txt --output JENKINS_VERSION
+#curl -s -L https://updates.jenkins.io/stable/latestCore.txt --output JENKINS_VERSION
+curl -s -Lhttps://updates.jenkins.io/current/latestCore.txt --output JENKINS_VERSION
 
 JENKINS_VERSION=`cat JENKINS_VERSION`
 LOCALREVISION=`cat VERSION | sed -re "s/^[0-9]+\.[0-9]+\.[0-9]+-*([0-9]*)/\1/" | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}'`
