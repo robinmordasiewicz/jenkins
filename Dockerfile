bb6301fd3eb7344e603d489d384f20b297edde35
robin@mordasiewicz.com <robin@mordasiewicz.com>
2022-04-03 01:41:03 -0400

    creating skel

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,8 @@
 FROM jenkins/jenkins:lts-jdk11
 <<<<<<< HEAD
-ENV JENKINS_VERSION 2.332.1-30
+ENV JENKINS_VERSION 2.332.1-42
 =======
-ENV JENKINS_VERSION 2.332.1-41
+ENV JENKINS_VERSION 2.332.1-42
 >>>>>>> 5d7fbae81470ccd9fd961df87ee40fb0989480a9
 ENV JAVA_OPTS -Djenkins.install.runSetupWizard=false
 
--- build.sh
+++ build.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 #
 
-<<<<<<< HEAD
-docker build -t robinhoodis/jenkins:2.332.1-30 .
-docker push robinhoodis/jenkins:2.332.1-30
-=======
-docker build -t robinhoodis/jenkins:2.332.1-41 .
-docker push robinhoodis/jenkins:2.332.1-41
->>>>>>> 5d7fbae81470ccd9fd961df87ee40fb0989480a9
+docker build -t robinhoodis/jenkins:2.332.1-42 .
+docker push robinhoodis/jenkins:2.332.1-42
